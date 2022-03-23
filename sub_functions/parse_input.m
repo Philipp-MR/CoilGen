fa724b680dde75d70eb4e3b5e03fc1eb5fe6f747
Philipp Amrein <philipp.amrein@uniklinik-freiburg.de>
2022-03-23 18:09:08 +0100

    changes

--- sub_functions/parse_input.m
+++ sub_functions/parse_input.m
@@ -13,17 +13,17 @@ addRequired(input_parser,'coil_mesh_file');
 addRequired(input_parser,'field_shape_function',@ischar);
 %offset factor for contour levels
 addParameter(input_parser,'pot_offset_factor',1/2,@isnumeric);
-%file of the coil surface mesh
+%file of the target surface mesh, which will define the target field
 addParameter(input_parser,'target_mesh_file','none',@ischar);
 %shielded_geometry_file; where the field should be supressed
 addParameter(input_parser,'secondary_target_mesh_file','none',@ischar);
-%shielded_geometry_file; where the field should be supressed
+%weight for the secondary target points 
 addParameter(input_parser,'secondary_target_weight',1,@isnumeric);
 % flag to use only the target mesh vertices as target coordinates
 addParameter(input_parser,'use_only_target_mesh_verts',false,@islogical);
 %file of an already optimized stream function
 addParameter(input_parser,'sf_source_file','none',@ischar);
-%Add list of reference files to find common potential range
+%Number of potential levels
 addParameter(input_parser,'levels',10,@isnumeric);
 %Specify one of the three ways the level sets are calculated:
 %"primary","combined", or "independent"
@@ -32,6 +32,8 @@ addParameter(input_parser,'level_set_method','primary',@ischar);
 addParameter(input_parser,'fieldtype_to_evaluate',"field",@isstring);
 % flag for cylindrical surface; in case of cylinder, a special parameterization will be used
 addParameter(input_parser,'surface_is_cylinder_flag',true,@islogical);
+%for the cylinder parameterization the ration of outer and inner boundary
+addParameter(input_parser,'circular_diameter_factor_cylinder_parameterization',1,@isnumeric);
 %the width in meter of the opening cut for the interconnection of the loops
 addParameter(input_parser,'interconnection_cut_width',0.01,@isnumeric);
 %Radius of a spherical target field
@@ -40,8 +42,6 @@ addParameter(input_parser,'target_region_radius',0.15,@isnumeric);
 addParameter(input_parser,'target_region_resolution',10,@isnumeric);
 % the distance in meter for which crossing lines will be seperated along the normal direction of the surface
 addParameter(input_parser,'normal_shift_length',0.001,@isnumeric);
-%for the cylinder parameterization the ration of outer and inner boundary
-addParameter(input_parser,'circular_diameter_factor_cylinder_parameterization',1,@isnumeric);
 % the minimal required number of point of a single loop; otherwise loops
 % will be removed..
 addParameter(input_parser,'min_point_loop_number',20,@isnumeric);
@@ -55,10 +55,9 @@ addParameter(input_parser,'max_allowed_angle_within_coil_track',120,@isnumeric);
 addParameter(input_parser,'min_allowed_angle_within_coil_track',0.0001,@isnumeric);
 %minimum relative percentage for which points will be deleted which contribute to segments which is extremly short
 addParameter(input_parser,'tiny_segment_length_percentage',0,@isnumeric);
-%  segment angle in degree above this parameter will be considered degenerate and therefore deleted
 %number of refinement iterations of the mesh together with the stream function
 addParameter(input_parser,'iteration_num_stream_func_refinement',0,@isnumeric);
-%the direction along the interconnection should be aligned
+%The direction (vector) along the interconnections will be aligned
 addParameter(input_parser,'b_0_direction',[0;0;1],@isnumeric);
 %In case of pcb layout, specify the track width
 addParameter(input_parser,'track_width_factor',0.5,@isnumeric);
@@ -68,19 +67,19 @@ addParameter(input_parser,'conductor_cross_section_width',0.002,@isnumeric);
 addParameter(input_parser,'conductor_cross_section_height',0.002,@isnumeric);
 %conducter conductiviy
 addParameter(input_parser,'specific_conductivity_conductor',0.018*10^(-6),@isnumeric);
-%conducter conductiviy
-addParameter(input_parser,'conductor_thickness',0.005,@isnumeric); % thickness of the sheet current density of within the stream function representation
-%cross_section_width of the conductor (for the inductance calculation) in meter
+% thickness of the sheet current density of within the stream function representation
+addParameter(input_parser,'conductor_thickness',0.005,@isnumeric); 
+%2D edge points for direct defintion of the cross section of the conductor
 addParameter(input_parser,'cross_sectional_points',[2 1.5 ;2 -1.5; -2 -1.5; -2 1.5; 2 1.5 ]'/2000,@isnumeric);
-%output directory
+%directory of the .stl  geometry files
 addParameter(input_parser,'geometry_source_path',strcat(pwd,'\','Geometry_Data'),@ischar);
-%Add list of reference files to find common potential range
+%output directory
 addParameter(input_parser,'output_directory',strcat(pwd,'\','Results'),@ischar);
 %flag to save sweeped .stl
 addParameter(input_parser,'save_stl_flag',false,@islogical);
 %flag to plot results
 addParameter(input_parser,'plot_flag',true,@islogical);
-%interconnection_method
+%interconnection_method: Regular or spiral in/out
 addParameter(input_parser,'interconnection_method','regular',@ischar);
 %Flag to skip post processing
 addParameter(input_parser,'skip_postprocessing',false,@islogical);
