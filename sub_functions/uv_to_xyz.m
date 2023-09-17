56666ae1164d9072683d9f74a451b63092c19a49
Philipp-MR <philipp.amrein@uniklinik-freiburg.de>
2023-09-17 13:35:45 +0200

    fix uv to xyz issue for high res meshes

--- sub_functions/uv_to_xyz.m
+++ sub_functions/uv_to_xyz.m
@@ -8,15 +8,15 @@ avg_mesh_diameter=norm([planary_mesh.Points-mean(planary_mesh.Points,1)]');
 
 for point_ind=1:size(points_in_2d,2)
 [target_triangle,bary_centric_coord] = pointLocation(planary_mesh,points_in_2d(1,point_ind-num_deleted_points),points_in_2d(2,point_ind-num_deleted_points));
-tri_inds=0;
-while isnan(target_triangle) % in case the point is directly between two triangles and no directly triangle indice can be found
-[target_triangle,bary_centric_coord] = pointLocation(planary_mesh,points_in_2d(1,point_ind-num_deleted_points)+ avg_mesh_diameter*(0.5-rand(1))/1000,points_in_2d(2,point_ind-num_deleted_points)+avg_mesh_diameter*(0.5-rand(1))/1000);
-tri_inds=tri_inds+1;
-if tri_inds>1000
-    fprintf(' Warning: Points cannot be assigned to a triangle \n');
-    break;
-end
-end
+% tri_inds=0;
+% while isnan(target_triangle) % in case the point is directly between two triangles and no directly triangle indice can be found
+% [target_triangle,bary_centric_coord] = pointLocation(planary_mesh,points_in_2d(1,point_ind-num_deleted_points)+ avg_mesh_diameter*(0.5-rand(1))/1000,points_in_2d(2,point_ind-num_deleted_points)+avg_mesh_diameter*(0.5-rand(1))/1000);
+% tri_inds=tri_inds+1;
+% if tri_inds>1000
+%     fprintf(' Warning: Points cannot be assigned to a triangle \n');
+%     break;
+% end
+% end
 if ~isnan(target_triangle)
 points_out_3d(:,point_ind-num_deleted_points) = barycentricToCartesian(curved_mesh,target_triangle,bary_centric_coord)';
 else %remove the point
